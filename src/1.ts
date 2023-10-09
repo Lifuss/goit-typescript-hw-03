07ae434ea751bd7f531436064c24463a5122fb5c
Arsen Hryhoriak <136361705+Lifuss@users.noreply.github.com>
2023-10-09 19:24:32 +0300

    hw-03

--- src/1.ts
+++ src/1.ts
@@ -5,15 +5,8 @@
 */
 
 class Student {
-  public name: string;
-  public age: number;
-  public grade: string;
-
-  constructor(name: string, age: number, grade: string) {
-    this.name = name;
-    this.age = age;
-    this.grade = grade;
-  }
+  // не вказав public бо вважаю що public іде по замовчуванню
+  constructor(name: string, age: number, grade: string) {}
 }
 
 export {};
--- src/4.ts
+++ src/4.ts
@@ -1,4 +1,44 @@
-const key = new Key();
+class Key {
+  constructor(private signature: number) {}
+  getSignature(): number {
+    return this.signature;
+  }
+}
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  private tenants: Person[] = [];
+  constructor(protected key: Key, public door: boolean) {}
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key, false);
+  }
+  openDoor(key: Key): void {
+    // я думаю що щось напартачив з рандомом, адже завжди буде одинакові значення, бо екземпляр класу key один
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+function randomNumber(): number {
+  return Math.floor(Math.random() * 10);
+}
+
+const key = new Key(randomNumber());
 
 const house = new MyHouse(key);
 const person = new Person(key);
@@ -6,6 +46,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
